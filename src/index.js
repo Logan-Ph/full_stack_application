5adfbfc601149dce3f4847ff139676bf6cf93e2e
Khang Nguyen <duykhang5687@gmail.com>
2023-05-07 20:37:06 +0700

    Added banner slider

--- src/index.js
+++ src/index.js
@@ -4,71 +4,66 @@ const path = require("path");
 const port = 3000;
 const hbs = require("hbs");
 const async = require("hbs/lib/async");
-const templatePath = path.join(__dirname,'../templates')
+
+const templatePath = path.join(__dirname, "../templates");
+const partialsPath = path.join(__dirname, "../templates/partials");
+const publicPath = path.join(__dirname, "../public");
+
 const collection = require("./mongodb");
 const { error } = require("console");
-const bcrypt = require('bcrypt')
-
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
-app.set("view engine", "hbs");
+app.use(express.static(publicPath));
+app.use(express.urlencoded({ extended: false }));
+
 app.set("views", templatePath);
-app.use(express.urlencoded({extended:false}));
+app.set("view engine", "hbs");
 
+hbs.registerPartials(partialsPath);
 
-app.get("/",(req,res) => {
-    res.render("login")
-})
+app.get("/", (req, res) => {
+  res.render("login");
+});
 
-app.get("/signup",(req,res) => {
-    res.render("signup")
-})
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
 
-app.post("/signup",async (req,res) => {
-    try{
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        console.log(salt);
-        console.log(hashedPassword);
-        const data={
-            name:req.body.name,
-            password:hashedPassword
-        }
-        await collection.insertMany([data]);
-    
-        res.render("login");
-        
+app.post("/signup", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    console.log(salt);
+    console.log(hashedPassword);
+    const data = {
+      name: req.body.name,
+      password: hashedPassword,
+    };
+    await collection.insertMany([data]);
+
+    res.render("login");
+  } catch {
+    console.log("error");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const check = await collection.findOne({ name: req.body.name });
+
+    if (await bcrypt.compare(req.body.password, check.password)) {
+      res.render("home");
+    } else {
+      res.send("Wrong password");
     }
+  } catch {
+    res.send("Wrong passwords or username");
+  }
 
-    catch{
-        console.log("error")
-    }
+  res.render("home");
+});
 
-
-})
-
-app.post("/login",async (req,res) => {
-    try{
-        const check = await collection.findOne({name:req.body.name})
-
-        if (await bcrypt.compare(req.body.password, check.password)){
-            res.render("home")
-        }
-        else{
-            res.send("Wrong password");
-        }
-    }
-
-    catch{
-        res.send("Wrong passwords or username");
-
-    }
-
-    res.render("home")
-
-})
-
-app.listen(port, ()=>{
-    console.log('port connected');
-})
-
+app.listen(port, () => {
+  console.log("port connected");
+});
