f3bcb8c053c76a93b02e8e32900360fd95398aea
Khang Nguyen <duykhang5687@gmail.com>
2023-05-19 18:23:06 +0700

    Updated database schema   - Affected file: mongodb.js

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,257 +1,231 @@
-const dotenv = require("dotenv")
-const path = require('path');
+const dotenv = require("dotenv");
+const path = require("path");
 // Init dotenv config path
-dotenv.config({ path: path.resolve(__dirname, '.', '.env') });
+dotenv.config({ path: path.resolve(__dirname, ".", ".env") });
 const mongoose = require("mongoose");
 // const mongoose = require("mongoose");
 // const connectionUrl = process.env.MONGODB_URL;
-const connectionUrl = "mongodb+srv://s3975979:Sang5850@cluster0.uuhro1a.mongodb.net/login?retryWrites=true&w=majority"
-
+const connectionUrl =
+  "mongodb+srv://s3975979:Sang5850@cluster0.uuhro1a.mongodb.net/login?retryWrites=true&w=majority";
 
 mongoose
-    .connect(connectionUrl)
-    .then(() => {
-        console.log("login database connected");
-    })
-    .catch(() => {
-        console.log("login database connected failed!");
-    });
+  .connect(connectionUrl)
+  .then(() => {
+    console.log("login database connected");
+  })
+  .catch(() => {
+    console.log("login database connected failed!");
+  });
 
 const LogInShipper = new mongoose.Schema({
-    username: {
-        type: String,
-        require: true,
-    },
+  username: {
+    type: String,
+    require: true,
+  },
 
-    password: {
-        type: String,
-        require: true,
-    },
+  password: {
+    type: String,
+    require: true,
+  },
 
-    address: {
-        type: String,
-        require: true,
-    },
+  address: {
+    type: String,
+    require: true,
+  },
 
-    distribution_hub: {
-        type: String,
-        enum: ["HCM", "HN", "DN"],
-        require: true,
-    },
+  distribution_hub: {
+    type: String,
+    enum: ["HCM", "HN", "DN"],
+    require: true,
+  },
 
-    phone_number: {
-        type: String,
-    },
+  phone_number: {
+    type: String,
+  },
 
-    img: {
-        data: Buffer,
-        contentType: String,
-    },
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
 });
 
 const LogInUser = new mongoose.Schema({
-    username: {
-        type: String,
-        require: true,
-    },
-    email: {
-        type: String,
-        require: false,
-    },
+  username: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: false,
+  },
 
-    password: {
-        type: String,
-        require: true,
-    },
+  password: {
+    type: String,
+    require: true,
+  },
 
-    address: {
-        type: String,
-        require: true,
-    },
+  address: {
+    type: String,
+    require: true,
+  },
 
-    name: {
-        type: String,
-        require: true,
-    },
+  name: {
+    type: String,
+    require: true,
+  },
 
-    phone_number: {
-        type: String,
-    },
+  phone_number: {
+    type: String,
+  },
 
-    img: {
-        data: Buffer,
-        contentType: String,
-    },
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
 });
 
 const LogInVendor = new mongoose.Schema({
-    username: {
-        type: String,
-        require: true,
-    },
+  username: {
+    type: String,
+    require: true,
+  },
 
-    password: {
-        type: String,
-        require: true,
-    },
+  password: {
+    type: String,
+    require: true,
+  },
 
-    bussiness_name: {
-        type: String,
-        require: true,
-    },
+  bussiness_name: {
+    type: String,
+    require: true,
+  },
 
-    bussiness_address: {
-        type: String,
-        require: true,
-    },
+  bussiness_address: {
+    type: String,
+    require: true,
+  },
 
-    phone_number: {
-        type: String,
-    },
+  phone_number: {
+    type: String,
+  },
 
-    img: {
-        data: Buffer,
-        contentType: String,
-    },
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
 });
 
 const Product = new mongoose.Schema({
-    owner: {
-        type: String,
-        require: true,
-    },
+  owner: {
+    type: String,
+    require: true,
+  },
 
-    product_name: {
-        type: String,
-        require: true,
-    },
+  product_name: {
+    type: String,
+    require: true,
+  },
 
-    category: {
-        type: String,
-        enum: ["cosmetics", "electronic-devices"],
-        require: true,
-    },
+  category: {
+    type: String,
+    enum: ["cosmetics", "electronic-devices"],
+    require: true,
+  },
 
-    price: {
-        type: Number,
-        require: true,
-    },
+  price: {
+    type: Number,
+    require: true,
+  },
 
-    description: {
-        type: String,
-        require: true,
-    },
+  description: {
+    type: String,
+    require: true,
+  },
 
-    img: {
-        data: Buffer,
-        contentType: String,
-    },
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
 });
 // Create index for searching products
 Product.index({ product_name: "text", description: "text" });
 
 const Ordered_Product = new mongoose.Schema({
-    customer_id: {
-        type: mongoose.Types.ObjectId,
-        ref: "Product",
-        require: true,
-    },
-    customer_id: {
-        type: mongoose.Types.ObjectId,
-        ref: "LogInUser",
-        require: true,
-    },
+  product_id: {
+    type: mongoose.Types.ObjectId,
+    ref: "Product",
+    require: true,
+  },
+  customer_id: {
+    type: mongoose.Types.ObjectId,
+    ref: "LogInUser",
+    require: true,
+  },
 
-    owner: {
-        type: String,
-        require: true,
-    },
+  owner: {
+    type: String,
+    require: true,
+  },
 
-    product_name: {
-        type: String,
-        require: true,
-    },
+  product_name: {
+    type: String,
+    require: true,
+  },
 
-    category: {
-        type: String,
-        enum: ["cosmetics", "electronic-devices"],
-        require: true,
-    },
+  category: {
+    type: String,
+    enum: ["cosmetics", "electronic-devices"],
+    require: true,
+  },
 
-    distribution_hub: {
-        type: String,
-        enum: ["HCM", "HN", "DN"],
-        require: true,
-    },
+  distribution_hub: {
+    type: String,
+    enum: ["HCM", "HN", "DN"],
+    require: true,
+  },
 
-    price: {
-        type: Number,
-        require: true,
-    },
+  price: {
+    type: Number,
+    require: true,
+  },
 
-    description: {
-        type: String,
-        require: true,
-    },
-
-    quantity: {
-        type: Number,
-        require: true,
-    },
-    img: {
-        data: Buffer,
-        contentType: String,
-    }
+  quantity: {
+    type: Number,
+    require: true,
+  },
 });
 
-
 const Cart = new mongoose.Schema({
-    customer_id: {
-        type: String,
-        require: true,
-    },
+  product_id: {
+    type: mongoose.Types.ObjectId,
+    ref: "Product",
+    require: true,
+  },
+  customer_id: {
+    type: mongoose.Types.ObjectId,
+    ref: "LogInUser",
+    require: true,
+  },
 
-    owner: {
-        type: String,
-        require: true,
-    },
+  owner: {
+    type: String,
+    require: true,
+  },
 
-    product_name: {
-        type: String,
-        require: true,
-    },
+  product_name: {
+    type: String,
+    require: true,
+  },
 
-    category: {
-        type: String,
-        enum: ["cosmetics", "electronic-devices"],
-        require: true,
-    },
+  price: {
+    type: Number,
+    require: true,
+  },
 
-    distribution_hub: {
-        type: String,
-        enum: ["HCM", "HN", "DN"],
-        require: true,
-    },
-
-    price: {
-        type: Number,
-        require: true,
-    },
-
-    description: {
-        type: String,
-        require: true,
-    },
-
-    img: {
-        data: Buffer,
-        contentType: String,
-    },
-
-    quantity: {
-        type: Number,
-        require: true,
-    },
+  quantity: {
+    type: Number,
+    require: true,
+  },
 });
 
 const shipper = new mongoose.model("LogInShipper", LogInShipper);
@@ -262,11 +236,11 @@ const ordered_product = new mongoose.model("Ordered_Product", Ordered_Product);
 const cart = new mongoose.model("Cart", Cart);
 
 module.exports = {
-    connectionUrl,
-    shipper,
-    user,
-    vendor,
-    product,
-    ordered_product,
-    cart
+  connectionUrl,
+  shipper,
+  user,
+  vendor,
+  product,
+  ordered_product,
+  cart,
 };
